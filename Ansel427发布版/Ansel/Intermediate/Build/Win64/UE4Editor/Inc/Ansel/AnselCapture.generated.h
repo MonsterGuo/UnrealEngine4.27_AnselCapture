658d38a904318b2abf707f607d55c37854e62af6
MonsterGuo <39860733+MonsterGuo@users.noreply.github.com>
2022-05-10 21:06:50 +0800

    打包发布版本

---
+++ Ansel427发布版/Ansel/Intermediate/Build/Win64/UE4Editor/Inc/Ansel/AnselCapture.generated.h
@@ -0,0 +1,111 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ANSEL_AnselCapture_generated_h
+#error "AnselCapture.generated.h already included, missing '#pragma once' in AnselCapture.h"
+#endif
+#define ANSEL_AnselCapture_generated_h
+
+#define HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_SPARSE_DATA
+#define HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_RPC_WRAPPERS \
+	virtual void CallSequncePlayer_Implementation(); \
+ \
+	DECLARE_FUNCTION(execTestCall); \
+	DECLARE_FUNCTION(execCallSequncePlayer);
+
+
+#define HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execTestCall); \
+	DECLARE_FUNCTION(execCallSequncePlayer);
+
+
+#define HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_EVENT_PARMS
+#define HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_CALLBACK_WRAPPERS
+#define HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAAnselCapture(); \
+	friend struct Z_Construct_UClass_AAnselCapture_Statics; \
+public: \
+	DECLARE_CLASS(AAnselCapture, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Ansel"), NO_API) \
+	DECLARE_SERIALIZER(AAnselCapture)
+
+
+#define HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_INCLASS \
+private: \
+	static void StaticRegisterNativesAAnselCapture(); \
+	friend struct Z_Construct_UClass_AAnselCapture_Statics; \
+public: \
+	DECLARE_CLASS(AAnselCapture, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Ansel"), NO_API) \
+	DECLARE_SERIALIZER(AAnselCapture)
+
+
+#define HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AAnselCapture(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AAnselCapture) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AAnselCapture); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AAnselCapture); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AAnselCapture(AAnselCapture&&); \
+	NO_API AAnselCapture(const AAnselCapture&); \
+public:
+
+
+#define HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AAnselCapture(AAnselCapture&&); \
+	NO_API AAnselCapture(const AAnselCapture&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AAnselCapture); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AAnselCapture); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AAnselCapture)
+
+
+#define HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_PRIVATE_PROPERTY_OFFSET
+#define HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_7_PROLOG \
+	HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_EVENT_PARMS
+
+
+#define HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_PRIVATE_PROPERTY_OFFSET \
+	HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_SPARSE_DATA \
+	HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_RPC_WRAPPERS \
+	HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_CALLBACK_WRAPPERS \
+	HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_INCLASS \
+	HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_PRIVATE_PROPERTY_OFFSET \
+	HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_SPARSE_DATA \
+	HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_RPC_WRAPPERS_NO_PURE_DECLS \
+	HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_CALLBACK_WRAPPERS \
+	HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_INCLASS_NO_PURE_DECLS \
+	HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ANSEL_API UClass* StaticClass<class AAnselCapture>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
